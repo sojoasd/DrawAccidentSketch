4620906f87d0bbbd7fc423e5a0538bfdf26214b6
zeal <sojoasd@gmail.com>
2015-11-20 17:35:24 +0800

    將 main2 的 slider 事件改成 callbackfunction

--- js/sliderbar.js
+++ js/sliderbar.js
@@ -1,45 +1,67 @@
+var SilderModel = function () {
+	
+	var _self = this;
+	
+	_self.$sliderbarDrag;
+	_self._sliderbardrag;
+	_self._LargerProportionNum;
+	
+	this.CreateCirclesSlider = function () {
+		$('.silder-bar').append('<div class="slider-margin-left"></div><div class="silder-bar-bg"><div class="silder-bar-lines"></div><div class="silder-bar-circles"></div></div><div class="slider-tips"></div>');
 
-$('.silder-bar').data("percents", { perX: 100 });
+		_self.$sliderbarDrag = $('.silder-bar-circles').draggabilly({
+			axis: 'x',
+			containment: true
+		});
 
-$('.silder-bar').append('<div class="slider-margin-left"></div><div class="silder-bar-bg"><div class="silder-bar-lines"></div><div class="silder-bar-circles"></div></div><div class="slider-tips"></div>');
+		_self._sliderbardrag = _self.$sliderbarDrag.data('draggabilly');
 
-var $sliderbarDrag = $('.silder-bar-circles').draggabilly({
-	axis: 'x',
-	containment: true
-});
+		_self._LargerProportionNum = 900;
 
-var _sliderbardrag = $sliderbarDrag.data('draggabilly');
+		_self.$sliderbarDrag.on('dragMove', function (event, pointer) {
+			var $el = $(this).parent().parent().find('.slider-tips');
+			$el.css({ 'opacity': 1.0 });
+			$el.html(" " + (100 + (_self._sliderbardrag.position.x / 500).toFixed(2) * _self._LargerProportionNum) + " % ");
+		});
+	};
 
-$sliderbarDrag.on('dragMove', function (event, pointer) {
-	var $el = $(this).parent().parent().find('.slider-tips');
-	$el.css({ 'opacity': 1.0 });
-	$el.html(" " + (100 + (_sliderbardrag.position.x / 500).toFixed(2) * 100) + " % ");
-});
+	this.circlesSlider = function (callback) {
+		_self.$sliderbarDrag.on('dragMove', function (event, pointer) {
+			var $el = $(this).parent().parent().find('.slider-tips');
+			$el.css({ 'opacity': 1.0 });
+			$el.html(" " + (100 + (_self._sliderbardrag.position.x / 500).toFixed(2) * _self._LargerProportionNum) + " % ");
+			callback((100 + (_self._sliderbardrag.position.x / 500).toFixed(2) * _self._LargerProportionNum));
+		});
 
-$('.silder-bar-circles').on({
-	mouseleave: function () {
-		var $el = $(this).parent().parent().find('.slider-tips');
-		$el.css({ 'opacity': 0.0 });
-	},
-	mouseover: function () {
-		var $el = $(this).parent().parent().find('.slider-tips');
-		$el.css({ 'opacity': 1.0 });
-		$el.html(" " + (100 + (_sliderbardrag.position.x / 500).toFixed(2) * 100) + " % ");
-		$('.silder-bar').data("percents", { perX: (100 + (_sliderbardrag.position.x / 500).toFixed(2) * 100) });
+		$('.silder-bar-circles').on({
+			mouseleave: function () {
+				var $el = $(this).parent().parent().find('.slider-tips');
+				$el.css({ 'opacity': 0.0 });
+			},
+			mouseover: function () {
+				var $el = $(this).parent().parent().find('.slider-tips');
+				$el.css({ 'opacity': 1.0 });
+				$el.html(" " + (100 + (_self._sliderbardrag.position.x / 500).toFixed(2) * _self._LargerProportionNum) + " % ");
+				$('.silder-bar').data("percents", { perX: (100 + (_self._sliderbardrag.position.x / 500).toFixed(2) * _self._LargerProportionNum) });
+				callback((100 + (_self._sliderbardrag.position.x / 500).toFixed(2) * _self._LargerProportionNum)); 
+				//因為 $('.silder-bar-lines').on() 事件結束後，球會跑到目的地，再觸發 mouseover 事件，就會回傳數值
+			}
+		});
+
+		$('.silder-bar-lines').on({
+			click: function (e) {
+				var nowX = _self._sliderbardrag.position.x;
+				var goalX = e.offsetX;
+				var moveLeft = goalX - nowX;
+				var circleWidth = $('.silder-bar-circles')[0].clientWidth / 2;
+				_self._sliderbardrag.position.x = goalX;
+				$('.silder-bar-circles').css({ 'left': nowX + moveLeft - circleWidth });
+
+				var $el = $(this).parent().parent().find('.slider-tips');
+				$el.css({ 'opacity': 1.0 });
+				$el.html(" " + _self._sliderbardrag.position.x + " % ");
+			}
+		});
 	}
-});
-
-$('.silder-bar-lines').on({
-	click: function (e) {
-		var nowX = _sliderbardrag.position.x;
-		var goalX = e.offsetX;
-		var moveLeft = goalX - nowX;
-		_sliderbardrag.position.x = goalX;
-		$('.silder-bar-circles').css({ 'left': nowX + moveLeft });
-
-		var $el = $(this).parent().parent().find('.slider-tips');
-		$el.css({ 'opacity': 1.0 });
-		$el.html(" " + _sliderbardrag.position.x + " % ");
-	}
-});
+};
 
